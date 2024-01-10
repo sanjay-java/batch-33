665e8d3bb9aea83ef5eda538f2e75d753494b35a
sangavi1510 <sangavik1510@gmail.com>
2024-01-09 21:46:04 -0600

    made changes

---
+++ Sangavikema.java
@@ -0,0 +1,5 @@
+class Sangavikema{
+    public static void main(String[] args) {
+        System.out.println("Hello sangavi kema");
+    }
+}
---
+++ sangavi.java
@@ -0,0 +1,7 @@
+https://github.com/sangavi1510/batch-33.git
+
+class sangavi{
+	public static void main(){
+		System.out.println("Hello sangavi kema");
+	}
+}
